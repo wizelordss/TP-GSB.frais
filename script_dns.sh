420d86e4ae6b152d1ce46332258c22fad973a9d6
wizelordss <91114160+wizelordss@users.noreply.github.com>
2021-09-22 21:19:03 +0200

    Create script_dns.sh

---
+++ script_dns.sh
@@ -0,0 +1,13 @@
+apt update -y
+apt install bind9 -y
+cd /etc
+rm host.conf
+rm resolv.conf
+wget https://raw.githubusercontent.com/wizelordss/TP-GSB.frais/main/host.conf
+wget https://raw.githubusercontent.com/wizelordss/TP-GSB.frais/main/resolv.conf
+cd bind/
+rm named.conf.local
+wget https://raw.githubusercontent.com/wizelordss/TP-GSB.frais/main/db.gsb.lan
+wget https://raw.githubusercontent.com/wizelordss/TP-GSB.frais/main/db.gsb.lan.rev
+wget https://raw.githubusercontent.com/wizelordss/TP-GSB.frais/main/named.conf.local
+systemctl restart bind
