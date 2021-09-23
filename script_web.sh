9f9ec5bba3e21db931db4617f1a9ad5216c3d99a
wizelordss <91114160+wizelordss@users.noreply.github.com>
2021-09-23 08:50:21 +0200

    Update script_web.sh

--- script_web.sh
+++ script_web.sh
@@ -1,5 +1,5 @@
 apt update && apt upgrade -y
-apt install apache2 libapache2-mod-php -y
+apt install apache2 libapache2-mod-php php-mysql-y
 apt install mariadb-server -y
 mysql_secure_installation
 cd /etc/apache2/sites-available/
@@ -11,11 +11,11 @@ mysql -e "CREATE DATABASE gsb_frais;"
 mysql -e "CREATE USER 'gsb'@'localhost' IDENTIFIED BY 'gsbpass';"
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'gsb'@'localhost';"
 mysql -e "flush privileges;"
-mysql -u root -p gsb_frais < /home/damien/gsb_frais_structure.sql
-mysql -u root -p gsb_frais < /home/damien/gsb_frais_insert_tables_statiques.sql
 wget https://raw.githubusercontent.com/wizelordss/TP-GSB.frais/main/gsb_frais_structure.sql
 wget https://raw.githubusercontent.com/wizelordss/TP-GSB.frais/main/gsb_frais_insert_tables_statiques.sql
+mysql -u root gsb_frais < gsb_frais_structure.sql
+mysql -u root gsb_frais < gsb_frais_insert_tables_statiques.sql
 cd /etc/apache2/sites-available/
 a2dissite 000-default.conf
 a2ensite gsbfrais.conf
-sytemctl restart apache2
+systemctl restart apache2
